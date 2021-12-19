09b3096e02070758b54e1d3ee02ac55328834e30
TheDukato <56132686+TheDukato@users.noreply.github.com>
2021-12-19 17:41:52 +0100

    Update BROKER.c

--- BROKER.c
+++ BROKER.c
@@ -72,7 +72,7 @@ hand_conn(int sockfd)
 again:
 	while ((n = read(sockfd, buf, MAXLINE)) > 0)
 		Writen(sockfd, buf, n);
-
+	printf("%c", buf);
 	if (n < 0 && errno == EINTR)
 		goto again;
 	else if (n < 0)
