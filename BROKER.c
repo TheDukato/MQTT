3604eded9338ac38c0fe5663c2d98df9a96cbf1b
TheDukato <56132686+TheDukato@users.noreply.github.com>
2021-12-22 13:07:12 +0100

    Update BROKER.c

--- BROKER.c
+++ BROKER.c
@@ -103,15 +103,13 @@ main(int argc, char** argv)
 		if ((childpid = fork()) == 0) {	/* child process */
 			close(listenfd);	/* close listening socket */
 			//Poczatek polaczenia
-			printf("Connection from %s as \n", str);
-//			rcv_fun(connfd);
+			printf("Connection from %s as ", str);
+			rcv_fun(connfd);
 			//Jeszcze dodanie zapisu do bazy jesli sub dopasowanie lista Subscribes [IP] [TOPIC] 
 			//
 			//
 			//
-			//for (int i = 0; i < 100;i++) {
 			send_time(connfd);	/* process the request */
-			//}
 			//rcv_msg(connfd);
 			exit(0);
 		}
