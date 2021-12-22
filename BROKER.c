bedb64fa4e04558bef747fdbc7f1ef55e3ea58d4
TheDukato <56132686+TheDukato@users.noreply.github.com>
2021-12-22 19:25:37 +0100

    Update BROKER.c

--- BROKER.c
+++ BROKER.c
@@ -98,7 +98,7 @@ main(int argc, char** argv)
 		}
 		////////////////////////////
 		int					n;
-		char				line[3];
+		char				line[4];
 		struct subscribers {
 			char IP[INET6_ADDRSTRLEN + 1];
 			char TOPIC[10];
