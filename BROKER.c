eaea8be4808da2714deaa48fefcf80d3d72f5df9
TheDukato <56132686+TheDukato@users.noreply.github.com>
2021-12-25 20:33:07 +0100

    Update BROKER.c

--- BROKER.c
+++ BROKER.c
@@ -120,7 +120,7 @@ main(int argc, char** argv)
 			//											 |
 			// 
 			//printf("%i", strcmp(&(pierwszy.TOPIC[0]), &(keywordSub1[0])));//funkcja do porownania stringow
-			printf("%i", strcmp(&(pierwszy.TOPIC[0]), &(keywordSub1[0])));
+			//printf("%i", strcmp(&(pierwszy.TOPIC[0]), &(keywordSub1[0])));
 			//printf("%i", strcmp(&(keywordSub2[0]), &(keywordSub1[0])));//
 			if (0==(strcmp(&(pierwszy.TOPIC[0]), &(keywordSub1[0])))) {///////////////////////////POROWNAIE STRING / CHAR
 				printf("Werification completed\n");
