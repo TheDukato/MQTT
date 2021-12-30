265226b3bf693915873404d39a737170687d65d7
TheDukato <56132686+TheDukato@users.noreply.github.com>
2021-12-30 20:01:36 +0100

    Update BROKER.c

--- BROKER.c
+++ BROKER.c
@@ -145,9 +145,9 @@ main(int argc, char** argv)
 			if (0 == (strcmp(&(fun[0]), &(keywordPub[0])))) {
 				printf("Publisher in topic ");
 				//Retrieved topic from message
-				for (int i = 3; i < (LENTOPIC + 3); i++) {
-					printf("%c", pierwszy.MSG[i]);
-				}	
+				//for (int i = 3; i < (LENTOPIC + 3); i++) {
+				//	printf("%c", pierwszy.MSG[i]);
+				//}	
 				//for (int i = (LENTOPIC + 3); i < MAXLINE; i++) {
 				//	printf("%c", pierwszy.MSG[i]);
 				//}
