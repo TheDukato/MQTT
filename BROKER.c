92ec75208906a2a609526b14221e6416cb2e05be
TheDukato <56132686+TheDukato@users.noreply.github.com>
2022-01-05 18:26:32 +0100

    Update BROKER.c

--- BROKER.c
+++ BROKER.c
@@ -120,7 +120,7 @@ doit(void* arg)
 			//printf("%c", pierwszy.MSG[i]);
 			topicsub[i - 3] = pierwszy.MSG[i];
 		}
-		printf("%s", topicsub);
+		printf("topicsub: %s\ntopic: %s\n%d", topicsub,topic, strcmp(&(topic[0]), &(topicsub[0])));
 		if (0 == (strcmp(&(topic[0]), &(topicsub[0])))) {
 			printf("\n%s\n", message);
 			snprintf(buff, sizeof(buff), "MSG: %s\r\n", message);
